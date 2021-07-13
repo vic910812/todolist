632397a8d7a1c4971bee40e2ad791e493b9141a9
Vic Huang <vic910812@gmail.com>
2021-07-13 15:11:11 +0800

    add mongoose

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+    console.log('mongodb error!')
+})
+
+db.once('open', () => {
+    console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
     res.send('hello world!')
 })
 
 app.listen(3000, () => {
-    console.log('App is running on port 3000.')
+    console.log('App is running on http://localhost:3000')
 })
