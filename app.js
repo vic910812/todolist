1c40b3d76e7b3dfec17b93aa00c970c906947e83
Vic Huang <vic910812@gmail.com>
2021-07-13 22:14:32 +0800

    add update function

--- app.js
+++ app.js
@@ -48,16 +48,36 @@ app.post('/todos', (req, res) => {
     //     .then(() => res.redirect('/'))
     //     .catch(error => console.log(error))
     return Todo.create({ name })
-       .then(() => res.redirect('/'))
-       .catch(error => console.log(error))
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error))
 })
 
 app.get('/todos/:id', (req, res) => {
     const id = req.params.id
     return Todo.findById(id)
-      .lean()
-      .then((todo) => res.render('detail', { todo }))
-      .catch(error => console.log(error))
+        .lean()
+        .then((todo) => res.render('detail', { todo }))
+        .catch(error => console.log(error))
+})
+
+app.get('/todos/:id/edit', (req, res) => {
+    const id = req.params.id
+    return Todo.findById(id)
+        .lean()
+        .then((todo) => res.render('edit', { todo }))
+        .catch(error => console.log(error))
+})
+
+app.post('/todos/:id/edit', (req, res) => {
+    const id = req.params.id
+    const name = req.body.name
+    return Todo.findById(id)
+        .then(todo => {
+            todo.name = name
+            return todo.save()
+        })
+        .then(() => res.redirect(`/todos/${id}`))
+        .catch(error => console.log(error))
 })
 
 app.listen(3000, () => {
