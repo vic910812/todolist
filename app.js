4acfee64e1ec703f4f61ea597d79a539ba2a8287
Vic Huang <vic910812@gmail.com>
2021-07-14 15:53:06 +0800

    add sort function

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.get('/', (req, res) => {
     //拿到全部的TODO資料
     Todo.find()
         .lean()
+        .sort({ _id: 'asc' }) //asc資料會按照順序，desc則是反序
         .then(todos => res.render('index', { todos: todos }))
         .catch(error => console.error(error))
 })
