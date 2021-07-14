0a4cad52888034b55c764693c4708e50c6dde7a6
Vic Huang <vic910812@gmail.com>
2021-07-14 16:58:33 +0800

    modify mongoose config

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,13 +1,5 @@
-const mongoose = require('mongoose')
 const Todo = require('../todo')
-
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-    console.log('mongodb error!')
-})
+const db = require('../../config/mongoose')
 
 db.once('open', () => {
     console.log('mongodb connected!')
