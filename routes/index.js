4b9b6d34e1c588b9d672ce721b51544eeae7ea48
Vic Huang <vic910812@gmail.com>
2021-07-16 17:16:54 +0800

    add user authentication in todo & home routers

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
 const express = require('express')
+const { authenticator } = require('../middleware/auth')
 const router = express.Router()
 
 const home = require('./modules/home')
 const todos = require('./modules/todos')
 const users = require('./modules/users')
 
-router.use('/', home)
-router.use('/todos', todos)
+const ( authenticator ) = require('../middleware/auth')
+
+//router機制是由上而下，若/擺在第一個，其他頁面就會被擋，所以條件寬鬆的route放後面
+router.use('/todos', authenticator, todos)
 router.use('/users', users)
+router.use('/', authenticator, home)
 
 module.exports = router
