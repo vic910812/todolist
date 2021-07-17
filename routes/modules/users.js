84411c6b8cc181c107f1e2853367c9ac7ad2dae4
Vic Huang <vic910812@gmail.com>
2021-07-17 12:50:31 +0800

    add message partials

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -10,8 +10,8 @@ router.get('/login', (req, res) => {
 })
 
 router.post('/login', passport.authenticate('local', {
-    successRedirect:'/',
-    failureRedirect:'users/login'
+    successRedirect: '/',
+    failureRedirect: 'users/login'
 }))
 
 router.get('/register', (req, res) => {
@@ -20,6 +20,23 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
     const { name, email, password, confirmPassword } = req.body
+    const errors = []
+
+    if (!name || !email || !password || !confirmPassword) {
+        errors.push({ message: '所有欄位都是必填。' })
+    }
+    if (password !== confirmPassword) {
+        errors.push({ message: '密碼與確認密碼不相符!' })
+    }
+    if (errors.length) {
+        return res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            confirmPassword
+        })
+    }
     //    以上用解構賦值可等同於下方四行   
     //    const name = req.body.name
     //    const email = req.body.email
@@ -27,35 +44,38 @@ router.post('/register', (req, res) => {
     //    const confirmPassword = req.body.comfirmPassword
     User.findOne({ email }).then(user => {
         if (user) {
-            console.log('User already exists.')
-            res.render('register', {
+            error.push({ message: '這個 Email 已經註冊過了。' })
+            return res.render('register', {
+                errors,
                 name,
                 email,
                 password,
                 confirmPassword
             })
-        } else {
-            return User.create({
-                name,
-                email,
-                password
-            })
-                //上放五行等同於下方
-                //   const newUser = new User({
-                //       name,
-                //       email,
-                //       password
-                //   })
-                //   newUser
-                //   .save()
-                .then(() => res.redirect('/'))
-                .catch(err => console.log(err))
         }
+
+        return User.create({
+            name,
+            email,
+            password
+        })
+            //上放五行等同於下方
+            //   const newUser = new User({
+            //       name,
+            //       email,
+            //       password
+            //   })
+            //   newUser
+            //   .save()
+            .then(() => res.redirect('/'))
+            .catch(err => console.log(err))
+
     })
 })
 
 router.get('/logout', (req, res) => {
     req.logout()
+    req.flash('success_msg', '你已經成功登出。')
     res.redirect('/users/login')
 })
 
