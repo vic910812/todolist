e67975283c7724c6c449b0f4bec50b97c98aad39
Vic Huang <vic910812@gmail.com>
2021-07-17 11:51:54 +0800

    add relations between User and Todo models

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,8 +4,9 @@ const router = express.Router()
 const Todo = require('../../models/todo')
 
 router.get('/', (req, res) => {
+    const userId = req.user._id
     //拿到全部的TODO資料
-    Todo.find()
+    Todo.find({ userId })
        .lean()
        .sort({ _id: 'asc' }) //asc資料會按照順序，desc則是反序
        .then(todos => res.render('index', { todos }))
