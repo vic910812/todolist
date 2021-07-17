9d95a774938046a64242b3ee4bd32be7107d9cba
Vic Huang <vic910812@gmail.com>
2021-07-17 13:17:07 +0800

    add bcrypt.js

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 
 const User = require('../../models/user')
 
@@ -54,19 +55,14 @@ router.post('/register', (req, res) => {
             })
         }
 
-        return User.create({
+        return bcrypt
+          .genSalt(10)
+          .then(salt => bcrypt.hash(password, salt))
+          .then(hash => User.create({
             name,
             email,
-            password
-        })
-            //上放五行等同於下方
-            //   const newUser = new User({
-            //       name,
-            //       email,
-            //       password
-            //   })
-            //   newUser
-            //   .save()
+            password: hash
+        }))
             .then(() => res.redirect('/'))
             .catch(err => console.log(err))
 
